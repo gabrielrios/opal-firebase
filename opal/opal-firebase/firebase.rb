3de666f1a218fc21a14a95bb93dfc09947234202
Gabriel Rios <gabrielfalcaorios@gmail.com>
2014-04-10 15:41:16 -0300

    Remove empty methods and try use alias_native wherever I can

--- opal/opal-firebase/firebase.rb
+++ opal/opal-firebase/firebase.rb
@@ -13,64 +13,37 @@ class Firebase
     `new Firebase(#{url})`
   end
 
-  # Authenticates a Firebase client to Firebase.
-  def auth
-  end
+  alias_native :authorize, :auth
+  alias_native :unauthorize, :unauth
+  alias_native :child, :child
+  alias_native :parent, :parent
+  alias_native :root, :root
+  alias_native :name, :name
+  alias_native :set, :set
+  alias_native :update, :update
+  alias_native :remove, :remove
+  alias_native :push, :push
+  alias_native :set_with_priority, :setWithPriority
+  alias_native :priority=, :setPriority
+  alias_native :transaction, :transaction
+  alias_native :off, :off
+  alias_native :once, :once
+  alias_native :limite, :limite
+  alias_native :start_at, :startAt
+  alias_native :end_at, :endAt
+  alias_native :go_online, :goOnline
+  alias_native :go_offlien, :goOffline
 
-  # Unauthenticates a Firebase client from Firebase.
-  def unauth
-  end
+  # # Write data into your Firebase.
+  # # TODO: Callback
+  # def set(value, &callback)
+  #   `#@native.set(#{value.to_n})`
+  # end
 
-  # Get a Firebase reference to a child location.
-  def child
-  end
-
-  # Get a Firebase reference for the parent location.
-  def parent
-  end
-
-  # Get a Firebase reference for the root of the Firebase.
-  def root
-  end
-
-  # Get the last token of this location's full URL.
-  def name
-  end
-
-  # Get the absolute URL corresponding to this location.
-  def toString
-  end
-
-  # Write data into your Firebase.
-  # TODO: Callback
-  def set(value, &callback)
-    `#@native.set(#{value.to_n})`
-  end
-
-  # Modify existing data in your Firebase.
-  def update
-  end
-
-  # Delete data from your Firebase.
-  def remove
-  end
-
-  # Add data to a list using a unique name.
-  def push(value, &callback)
-    `#@native.push(#{value.to_n})`
-  end
-
-  # Write data into your Firebase using a specific priority.
-  def set_with_priority
-  end
-
-  # Update the priority of data in your Firebase.
-  def priority=
-  end
-
-  # Atomically modify data.
-  def transaction
-  end
+  # # Add data to a list using a unique name.
+  # def push(value, &callback)
+  #   `#@native.push(#{value.to_n})`
+  # end
 
   # Attach a callback to read data and receive data changes.
   # Based on
@@ -84,23 +57,12 @@ class Firebase
     `#@native.on(#{event_type}, #{wrapper})`
   end
 
-  # Remove a callback attached with on #.
-  def off
+  # Get the absolute URL corresponding to this location.
+  def to_s
+    url
   end
 
-  # Attach a callback that will only be triggered once.
-  def once
-  end
-
-  # Create a Query limited to a maximum number of children.
-  def limit
-  end
-
-  # Create a Query with the specified starting point.
-  def start_at
-  end
-
-  # Create a Query with the specified ending point.
-  def end_at
+  def inspect
+    "#<Firebase url: #{url}>"
   end
 end
